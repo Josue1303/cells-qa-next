6aad086b69f7acf56adeb3d2782290e62ff8dc03
LG <92387298+LGnan@users.noreply.github.com>
2024-05-26 19:31:22 -0600

    Dedos finales

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -9,7 +9,7 @@ import Modal from "./Modal";
 import Modal2 from "./Modal2";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   // console.log(data);
   const [teams, setTeams] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
@@ -18,20 +18,20 @@ export default function Home() {
   const [isModalOpen3, setIsModalOpen3] = useState(false);
   const [selectedTeamId, setSelectedTeamId] = useState(null);
 
-  // useEffect(() => {
-  //   if (status === "authenticated") {
-  //     fetchUserTeams();
-  //   }
-  // }, [status]);
+  useEffect(() => {
+    if (status === "authenticated") {
+      fetchUserTeams();
+    }
+  }, [status]);
 
   const fetchUserTeams = async () => {
     try {
-      const userId = 36;
-      console.log("Aqui Inicia");
-      console.log(session.user.email);
-      console.log(session.user.id);
-      console.log(session.user.username);
-      console.log("Termian");
+      const userId = session?.user?.id;
+      // console.log("Aqui Inicia");
+      // console.log(session.user.email);
+      // console.log(session.user.id);
+      // console.log(session.user.username);
+      // console.log("Termian");
       if (!userId) {
         return;
       }
@@ -69,7 +69,7 @@ export default function Home() {
 
   const handleDelete = async () => {
     try {
-      // const userId = session?.user?.id;
+      const userId = session?.user?.id;
       if (!userId) {
         return;
       }
